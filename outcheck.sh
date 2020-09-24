5eb89662232317bfd94f9a154e89bb27aa543060
unknown <andras5000@gmail.com>
2020-09-24 18:06:58 +0200

    removed unnessecary new line insertion from output

--- outcheck.sh
+++ outcheck.sh
@@ -4,16 +4,10 @@
 touch output.txt
 
 ./$1 Units/a.txt Units/b.txt > output.txt
-echo "" >> output.txt
 ./$1 Units/a.txt Units/c.txt >> output.txt
-echo "" >> output.txt
 ./$1 Units/b.txt Units/a.txt >> output.txt
-echo "" >> output.txt
 ./$1 Units/b.txt Units/c.txt >> output.txt
-echo "" >> output.txt
 ./$1 Units/c.txt Units/a.txt >> output.txt
-echo "" >> output.txt
 ./$1 Units/c.txt Units/b.txt >> output.txt
-echo "" >> output.txt
 
 diff output.txt $2
