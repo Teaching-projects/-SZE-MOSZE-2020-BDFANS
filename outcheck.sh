ea5d1125f350a32f95c9d9d8a322f79ff231a905
unknown <andras5000@gmail.com>
2020-09-24 18:05:14 +0200

    fixed file access casesensitivity for real...

--- outcheck.sh
+++ outcheck.sh
@@ -3,17 +3,17 @@
 
 touch output.txt
 
-./$1 Units/a.txt units/b.txt > output.txt
+./$1 Units/a.txt Units/b.txt > output.txt
 echo "" >> output.txt
-./$1 Units/a.txt units/c.txt >> output.txt
+./$1 Units/a.txt Units/c.txt >> output.txt
 echo "" >> output.txt
-./$1 Units/b.txt units/a.txt >> output.txt
+./$1 Units/b.txt Units/a.txt >> output.txt
 echo "" >> output.txt
-./$1 Units/b.txt units/c.txt >> output.txt
+./$1 Units/b.txt Units/c.txt >> output.txt
 echo "" >> output.txt
-./$1 Units/c.txt units/a.txt >> output.txt
+./$1 Units/c.txt Units/a.txt >> output.txt
 echo "" >> output.txt
-./$1 Units/c.txt units/b.txt >> output.txt
+./$1 Units/c.txt Units/b.txt >> output.txt
 echo "" >> output.txt
 
 diff output.txt $2
