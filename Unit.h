5a80ea657ccbedf72709a2319949c423989e7ea5
LuczaDennis <70800404+LuczaDennis@users.noreply.github.com>
2020-10-10 19:44:15 +0200

    Update Unit.h

--- Unit.h
+++ Unit.h
@@ -4,20 +4,28 @@
 class Unit
 {
 	const std::string name;
-	int health;
-	const int damage;
+	int maxhealth;
+	int akthealth=maxhealth;
+	int damage;
+	int exp=0;
+	int lvl=1;
 
 
 public:
 
 	std::string getName() const { return name; }
-	int getHealth() const { return health; }
+	int getaktHealth() const { return akthealth; }
+	int getmaxHealth() const { return maxhealth; }
 	int getDamage() const { return damage; }
 
+	int getExp() const { return exp; }
+	void gainExp(int xp);
+	void setUnit(Unit player);
+	void setAkthp(int i) { akthealth = i; }
+	int getLvl() const { return lvl; }
+	Unit(const std::string &inn = "default", int inh = 1, int ind = 1) :name(inn), maxhealth(inh), damage(ind) {}
 
-	Unit(std::string inn = "default", int inh = 1, int ind = 1) :name(inn), health(inh), damage(ind) {}
-
-	void attack(Unit& target) const;
+	int attack(Unit& target) const;
 
 	friend std::ostream& operator<<(std::ostream& os, const Unit& out);
 
