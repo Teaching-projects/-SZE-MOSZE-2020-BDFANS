7e7d605e602764483fa2fe0d13c5992ad901849d
unknown <andras5000@gmail.com>
2020-10-13 18:48:52 +0200

    parseUnit() check if keys exist

--- Unit.cpp
+++ Unit.cpp
@@ -28,12 +28,27 @@ Unit* Unit::praseUnit(std::string fnev) {
 	try
 	{
 		std::map<std::string,std::string> data = jsonparser::jsonparse_f(fnev);
-
-		std::string innev = data.find("name")->second;
+		std::string innev = "default";
+		int inhp = 1;
+		int indmg = 1;
+		//double inattackcd = 1;
+		if(data.count("name") > 0)
+		{
+		innev = data.find("name")->second;
+		}
+		if(data.count("hp") > 0)
+		{
 		int inhp = stoi(data.find("hp")->second);
+		}
+		if(data.count("dmg") > 0)
+		{
 		int indmg = stoi(data.find("dmg")->second);
+		}
 		/* attackcooldown
+		if(data.count("attackcooldown") > 0)
+		{
 		double inattackcd = stod(data,find("attackcooldown")-second);
+		}
 		return new Unit(innev,inhp,indmg,inattackcd);
 		*/
 		return new Unit(innev,inhp,indmg);
