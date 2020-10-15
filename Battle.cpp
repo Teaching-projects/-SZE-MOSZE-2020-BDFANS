60f7960b4c2c47b601285d8924cc932e774504ef
unknown <andras5000@gmail.com>
2020-10-15 20:53:56 +0200

    initial release

--- Battle.cpp
+++ Battle.cpp
@@ -2,25 +2,20 @@
 
 void Battle(Unit& attacker, Unit& defender)
 {
-	while (true)
-	{
-
-		attacker.attack(defender);
-
-		if (defender.getHealth() > 0)
-		{
-			defender.attack(attacker);
-
-			if (attacker.getHealth() <= 0)
-			{
-				std::cout << defender.getName() << " wins. Remaining HP: " << defender.getHealth() << ".\n";
-				break;
-			}
-		}
-		else
-		{
-			std::cout << attacker.getName() << " wins. Remaining HP: " << attacker.getHealth() << ".\n";
-			break;
-		}
+	std::cout << "Attacker attacks/second " << attacker.getAttackspeed() << std::endl;
+	std::cout << "Defender attacks/second " << defender.getAttackspeed() << std::endl;
+	while (attacker.getHealth() > 0 && defender.getHealth() > 0)
+	{	
+		Unit::attackcd(attacker,defender);
 	}
+
+	if(attacker.getHealth() > 0)
+	{
+		std::cout << attacker.getName() << " wins. Remaining HP: " << attacker.getHealth() << ".\n";
+	}
+	else
+	{
+		std::cout << defender.getName() << " wins. Remaining HP: " << defender.getHealth() << ".\n";
+	}
+	
 }
--- Unit.cpp
+++ Unit.cpp
@@ -15,6 +15,36 @@ void Unit::attack(Unit& target) const
 	}
 }
 
+void Unit::setAttackCooldown(double in)
+{
+	if(in >= 0)
+	{
+		attackcooldown = in;
+	}
+	else
+	{
+		attackcooldown = 0;
+	}
+	
+
+}
+
+void Unit::attackcd(Unit& attacker, Unit& defender)
+{
+	if(attacker.attackcooldown <= defender.attackcooldown)
+	{
+		defender.setAttackCooldown(defender.attackcooldown - attacker.attackcooldown);
+		attacker.attackcooldown = 1/attacker.attackspeed;
+		attacker.attack(defender);
+	}
+	else
+	{
+		attacker.setAttackCooldown(attacker.attackcooldown - defender.attackcooldown);
+		defender.attackcooldown = 1/defender.attackspeed;
+		defender.attack(attacker);
+	}
+}
+
 std::ostream& operator<<(std::ostream& os, const Unit& out)
 {
 	os << out.getName() << ": HP: " << out.getHealth() << " DMG: " << out.getDamage() << std::endl;
@@ -27,6 +57,7 @@ Unit* Unit::praseUnit(std::string fnev) {
 	if (fin) {
 		int health, dmg;
 		std::string x, name;
+		double atckspd;
 		while (!fin.eof()) {
 			std::getline(fin, x);
 			std::string s;
@@ -48,9 +79,12 @@ Unit* Unit::praseUnit(std::string fnev) {
 			else if (s.find("dmg") != std::string::npos) {
 				dmg = stoi(s.substr(3));
 			}
+			else if (s.find("attackcooldown") != std::string::npos) {
+				atckspd = stod(s.substr(14));
+			}
 		}
 		fin.close();
-		return new Unit(name, health, dmg);
+		return new Unit(name, health, dmg, atckspd);
 	}
 	else {
 		throw std::exception();
