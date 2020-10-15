83d45d23a520493b6107241dbd944d5a31e5ebdd
unknown <andras5000@gmail.com>
2020-10-15 13:11:19 +0200

    remplaced gtestinit.sh with direct funcion calls

--- gtestinit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-apt install libgtest-dev 
-cd /usr/src/gtest 
-cmake CMakeLists.txt 
-make 
-ln -s /usr/lib/ /usr/src/gtest/libgtest.a 
-ln -s /usr/lib/ /usr/src/gtest/libgtest_main.a
