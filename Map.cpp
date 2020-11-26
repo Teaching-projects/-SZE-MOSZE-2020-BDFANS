4fb3ed6861d6f71da51f755bcd8bc89c1c382c01
unknown <andras5000@gmail.com>
2020-11-26 15:39:23 +0100

    Initial Map release

---
+++ Map.cpp
@@ -0,0 +1,75 @@
+#include "Map.h"
+
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+
+/**
+ * @brief Construct a new Map:: Map object
+ * 
+ * @param filename the name of the file used to generate the map
+ */
+Map::Map(std::string filename)
+{
+    std::ifstream infile(filename);
+    if(infile)
+    {
+        std::string instring;
+        //generate initial map
+        int inX = 0;
+        int inY = 0;
+        while(std::getline(infile,instring))
+        {
+            inY++;
+            inX = std::max(inX,(int)instring.length());
+        }
+        lenX = inX;
+        lenY = inY;
+        loadedmap = MAPMATRIX(lenY,std::vector<type>(lenX,type::Wall));
+
+        //load map features
+        infile.clear();
+        infile.seekg(0);
+        inY = 0;
+        while(std::getline(infile,instring))
+        {
+            for(inX = 0; inX < instring.length(); inX++)
+            {
+                switch(instring[inX])
+                {
+                case '#':
+                    loadedmap[inY][inX] = type::Wall;
+                    break;
+
+                case ' ':
+                    loadedmap[inY][inX] = type::Free;
+                    break;
+
+                default:
+                    break;
+                }
+            }
+            inY++;
+        }
+        infile.close();
+    }
+    else
+    {
+        throw std::exception();
+    }
+
+}    
+
+
+type Map::get(int x, int y) const
+{
+    if(x < lenX && y < lenY)
+    {
+         return loadedmap[y][x];
+    }
+    else
+    {
+        throw WrongIndexException();
+    }
+}
---
+++ Map.h
@@ -0,0 +1,64 @@
+/**
+ * @file Map.h
+ * @class Map
+ *
+ * @author BDFANS
+ *
+ * @brief header for Map class
+ * @version 0.4.0
+ *
+ *
+ * @date 2020-11-13
+ *
+ */
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+
+#define MAPMATRIX std::vector<std::vector<type>> 
+
+///This enum represents the map features(Free tiles and Walls)
+enum type {Free,Wall};
+
+class Map
+{
+    ///The exception thrown when the map is indexed outside of its bounds
+    class WrongIndexException :public std::exception {  };
+
+    //The actual map stored in a vector matrix of "type"
+    MAPMATRIX loadedmap;
+    ///Width of the map
+    int lenX;
+    ///Length of the map
+    int lenY;
+
+
+public:
+
+    Map(std::string filename);
+
+    /**
+     * @brief Return the type of tile located at the coordinates
+     * 
+     * @param x the X coordinates
+     * @param y the Y coordinates
+     * @return type the type of the tile
+     */
+    type get(int x, int y) const;
+
+    ///return the width of the map
+    int getlenX() const
+    {
+        return lenX;
+    }
+
+    ///returns the length of the map
+    int getlenY() const
+    {
+        return lenY;
+    }
+
+
+};
