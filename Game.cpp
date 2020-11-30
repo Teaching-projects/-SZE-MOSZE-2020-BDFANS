b0ec653c414ea4768862b326ed39507916dcb4bf
LuczaDennis <70800404+LuczaDennis@users.noreply.github.com>
2020-11-30 19:57:46 +0100

    Fixing constructor

--- Game.cpp
+++ Game.cpp
@@ -1,7 +1,7 @@
 
 #include "Game.h"
 
-Game::Game(std::string mapfilenev) :hero(), isMapSet(false), isHeroSet(false), isMonsterSet(false), isStarted(false), {
+Game::Game(std::string mapfilenev) :hero(), isMapSet(false), isHeroSet(false), isMonsterSet(false), isStarted(false) {
 
 	Map newMap(mapfilenev);
 	setMap(newMap);
