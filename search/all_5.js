d6f302c199fdceca7729cdb47b17c91d048fc63a
LuczaDennis <70800404+LuczaDennis@users.noreply.github.com>
2020-12-01 23:13:38 +0100

    Update gh-pages to output generated at 4bc1c6b

--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['hero_29',['Hero',['../class_hero.html',1,'Hero'],['../class_hero.html#a49bde3a09b12953e7ba35dc2356b3df2',1,'Hero::Hero()']]],
+  ['hero_29',['Hero',['../class_hero.html',1,'Hero'],['../class_hero.html#ab5920677a4b5cb59d6f513922d037dca',1,'Hero::Hero()'],['../class_hero.html#a49bde3a09b12953e7ba35dc2356b3df2',1,'Hero::Hero(std::string innev, int inhp, int inphys, int inmagic, int indef, double inaspeed, int inexpreq=1, int inhpb=1, int inphysb=1, int inmagicb=1, int indefb=1, double incdmul=1)']]],
   ['hero_2eh_30',['Hero.h',['../_hero_8h.html',1,'']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['hero_103',['Hero',['../class_hero.html#a49bde3a09b12953e7ba35dc2356b3df2',1,'Hero']]]
+  ['hero_103',['Hero',['../class_hero.html#ab5920677a4b5cb59d6f513922d037dca',1,'Hero::Hero()'],['../class_hero.html#a49bde3a09b12953e7ba35dc2356b3df2',1,'Hero::Hero(std::string innev, int inhp, int inphys, int inmagic, int indef, double inaspeed, int inexpreq=1, int inhpb=1, int inphysb=1, int inmagicb=1, int indefb=1, double incdmul=1)']]]
 ];
